904e85ba9bbc0a6fa02f5ea3c244b856f39b768a
jinzhu <wangjinzhulala@gmail.com>
2024-04-07 13:45:25 +1000

    Compute the urban are for each year with bincount

--- 08_Get_future_urban_area.py
+++ 08_Get_future_urban_area.py
@@ -9,51 +9,25 @@ from helper_func.parameters import HDF_BLOCK_SIZE, UNIQUE_VALUES, Province_names
 from dask.diagnostics import ProgressBar
 
 
-# Read the lucc mask
-lucc_mask_ds =  h5py.File('data/LUCC/LUCC_Province_mask.hdf5', 'r')
-lucc_mask_arr = da.from_array(lucc_mask_ds['Array'], chunks=lucc_mask_ds['Array'].chunks)
-# lucc_mask_arr_sparsed = lucc_mask_arr[:, ::10, ::10].compute()
-
-
 # Read the lucc area
 lucc_area_ds = h5py.File('data/LUCC/LUCC_area_km2.hdf5', 'r')
 lucc_area_area = da.from_array(lucc_area_ds['area'], chunks=lucc_area_ds['area'].chunks) 
-# lucc_area_area_sparsed = lucc_area_area[::10, ::10].compute()
-lucc_area_area = lucc_area_area * lucc_mask_arr 
 
 
 # Read the urban data
 urban_ds = h5py.File('data/LUCC/Urban_1990_2019.hdf5', 'r')
-urban_arr = da.from_array(urban_ds['Array'], chunks=urban_ds['Array'].chunks)
-# urban_arr_sparsed = urban_arr[:, ::10, ::10].compute()
-urban_arr = da.where(lucc_mask_arr, urban_arr, 0)
+urban_arr = da.from_array(urban_ds['Array'], chunks=urban_ds['Array'].chunks)[0]
+
+
+with dask.config.set(**{'array.slicing.split_large_chunks': True}), ProgressBar():
+    # Compute the bincount for the province and append it to the list
+    area = da.bincount(urban_arr.ravel(), weights=lucc_area_area.ravel()).compute()
 
 
 
 
 
 
-# Get the urban area for each province with the bincount function
-with ProgressBar():
-    area_each_province = da.map_blocks(da.bincount, lucc_area_area_sparsed.ravel(), weights=urban_arr_sparsed.ravel(), dtype=np.float64).compute()
-
-
-
-tt = np.bincount(urban_arr_sparsed.astype(np.uint8).ravel(), weights=lucc_area_area_sparsed.ravel() )
-
-
-
-
-
-# Initialize an empty list to store the counts for each province
-counts_per_province = []
-
-# Iterate over the first dimension of urban_arr
-for arr in urban_arr:
-    with dask.config.set(**{'array.slicing.split_large_chunks': True}):
-        # Compute the bincount for the province and append it to the list
-        counts = da.bincount(arr.ravel()).compute()
-        counts_per_province.append(counts)
 
 
 
