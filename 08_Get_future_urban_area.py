04a5d5df06805479a0102b9eae3d9debe0faf4b0
Jinzhu WANG <wangjinzhulala@gmail.com>
2024-05-02 14:33:07 +1000

    Correct the bincount for hist and potential cumsum area

--- 08_Get_future_urban_area.py
+++ 08_Get_future_urban_area.py
@@ -9,7 +9,7 @@ from helper_func.parameters import UNIQUE_VALUES, HDF_BLOCK_SIZE
 from dask.diagnostics import ProgressBar
 
 # Define the working chunk size
-work_size = HDF_BLOCK_SIZE * 32
+work_size = HDF_BLOCK_SIZE * 8
 
 
 # Read the lucc mask
@@ -35,8 +35,10 @@ urban_potential_arr = urban_potential_arr.squeeze(0)
 
 
 # Encode year and province to a single array
-year_base = 100
-urban_year_region = urban_arr * year_base + region_arr
+year_base = 10
+potential_base = 10000
+urban_year_region = region_arr.astype(np.uint32) * year_base + urban_arr
+urban_potential_region = region_arr.astype(np.uint32) * potential_base + urban_potential_arr.astype(np.uint32)
 
 
 
@@ -44,44 +46,44 @@ urban_year_region = urban_arr * year_base + region_arr
 with ProgressBar():
     # Compute the bincount for the province and append it to the list
     area_hist = da.bincount(urban_year_region.ravel(), weights=lucc_area_area.ravel())
-    area_potential = da.bincount(urban_potential_arr.ravel(), weights=lucc_area_area.ravel())
+    area_potential = da.bincount(urban_potential_region.ravel(), weights=lucc_area_area.ravel())
     area_hist, area_potential = dask.compute(area_hist, area_potential)
     
 
 
 
+# Get the historical urban area
+area_hist_df = pd.DataFrame(enumerate(area_hist), columns=['encode', 'Area_km2'])
+area_hist_df['Province_code'] = area_hist_df['encode'] // year_base
+area_hist_df['year_code'] = area_hist_df['encode'] % year_base
+
+area_hist_df['Province'] = area_hist_df['Province_code'].map(dict(enumerate(UNIQUE_VALUES['Province'])))
+area_hist_df['year'] = area_hist_df['year_code'].map({k+1:v for k, v in enumerate(UNIQUE_VALUES['Urban_map_year'][::-1])})
+
+area_hist_df = area_hist_df.dropna()
+area_hist_df['year'] = area_hist_df['year'].astype(int)
+
+area_hist_df = area_hist_df.sort_values(['Province', 'year'])
+area_hist_df['Area_cumsum_km2'] = area_hist_df.groupby('Province')['Area_km2'].cumsum()
+area_hist_df = area_hist_df[['Province', 'year', 'Area_cumsum_km2']]
 
 
+# Get the potential urban area
+urban_area_potential = pd.DataFrame(enumerate(area_potential), columns=['encode', 'Area_km2'])
+urban_area_potential['Province_code'] = urban_area_potential['encode'] // potential_base
+urban_area_potential['Potential'] = urban_area_potential['encode'] % potential_base
 
+urban_area_potential['Province'] = urban_area_potential['Province_code'].map(dict(enumerate(UNIQUE_VALUES['Province'])))
+urban_area_potential = urban_area_potential.sort_values(['Province', 'Potential'], ascending=[True, False])
 
+urban_area_potential = urban_area_potential.dropna().reset_index(drop=True)
+urban_area_potential['Area_cumsum_km2'] = urban_area_potential.groupby('Province')['Area_km2'].cumsum()
+urban_area_potential = urban_area_potential[['Province', 'Potential', 'Area_cumsum_km2']]
 
-
-df_hists, df_potentials = [], []
-for d in area_each_province:
-    # Get the keys you want to retrieve
-    key_hist = ['Province', 'Area_hist']
-    key_potential = ['Province', 'Area_potential']
-    # Crete the df
-    df_hist = pd.DataFrame({key: d[key] for key in key_hist})
-    df_potential = pd.DataFrame({key: d[key] for key in key_potential})
-    # Add year to hist and pixel-val to potential
-    df_hist['year'] = UNIQUE_VALUES['Urban_map_year']
-    df_potential['pixel_val'] = np.arange(1, len(df_potential) + 1)
-    df_potential = df_potential.sort_values('Area_potential', ascending=False)
-    # Cumsum the value
-    df_hist['Area_cumsum_km2'] = df_hist['Area_hist'].cumsum()
-    df_potential['Area_cumsum_km2'] = df_potential['Area_potential'].cumsum()
-    # Append the df to the list
-    df_hists.append(df_hist)
-    df_potentials.append(df_potential)
-
-# Concatenate the list to a single df
-urban_area_hist = pd.concat(df_hists)
-urban_area_potential = pd.concat(df_potentials).reset_index(drop=True)
 
 
 # Convert the list to a Dask array
-urban_area_hist.to_csv('data/results/urban_area_hist.csv', index=False)
+area_hist_df.to_csv('data/results/area_hist_df.csv', index=False)
 urban_area_potential.to_csv('data/results/urban_area_potential.csv', index=False)
 
 # Read the data
@@ -95,15 +97,15 @@ if __name__ == '__main__':
     plotnine.options.figure_size = (12, 8)
     plotnine.options.dpi = 100
     
-    g = (plotnine.ggplot(urban_area_hist) +
+    g = (plotnine.ggplot(area_hist_df) +
         plotnine.aes(x='year', y='Area_cumsum_km2', color='Province') +
         plotnine.geom_line() +
         plotnine.theme_bw() +
         plotnine.theme(axis_text_x=plotnine.element_text(rotation=45, hjust=1))
         )
     
-    g = (plotnine.ggplot(urban_area_potential) +
-        plotnine.aes(x='pixel_val', y='Area_cumsum_km2', color='Province') +
+    g = (plotnine.ggplot(urban_area_potential.query('Potential > 9900')) +
+        plotnine.aes(x='Potential', y='Area_cumsum_km2', color='Province') +
         plotnine.geom_line() +
         plotnine.theme_bw() +
         plotnine.theme(axis_text_x=plotnine.element_text(rotation=45, hjust=1)) +
