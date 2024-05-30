4640f3002e18f2c4c1f24fa33be7417039fcb827
jinzhu <wangjinzhulala@gmail.com>
2024-05-30 15:05:35 +1000

    Calculate area_km raster in nc format

--- data_transform/04_Calculate_TIF_area_km2.py
+++ data_transform/04_Calculate_TIF_area_km2.py
@@ -1,5 +1,6 @@
 import rasterio
 import xarray as xr
+import rioxarray as rxr
 import numpy as np
 import rasterio
 
@@ -13,38 +14,16 @@ from helper_func.parameters import BLOCK_SIZE
 
 # Save the GAEZ_tif area
 tif = glob('data/GAEZ_v4/GAEZ_tifs/*tif')[0]
-output_path = 'data/GAEZ_v4/GAEZ_area_km2.tif'
-meta, arr = calculate_area(tif)
+output_path = 'data/GAEZ_v4/GAEZ_area_km2.nc'
+calculate_area(path=tif, output_path=output_path)
 
-arr = xr.DataArray(arr,dims=['y','x'])
-arr = arr.rio.write_crs(meta['crs'])
-arr = arr.rio.write_transform(meta['transform'])
 
 
 # Save the LUCC_tif area
-tif = 'data/LUCC/Norm_Urban_1990_2019.tif'
-output_path = 'data/LUCC/LUCC_area_km2.tif'
+path = 'data/LUCC/Norm_Urban_1990_2019.nc'
+output_path = 'data/LUCC/LUCC_area_km2.nc'
 work_size = BLOCK_SIZE * 30
-meta, arr = calculate_area(tif, chunk_size=work_size)
-
-arr = xr.DataArray(arr, dims=['y','x'])
-arr = arr.rio.write_crs(meta['crs'])
-arr = arr.rio.write_transform(meta['transform'])
-
-# Get the chunks
-chunk_sizes = arr.chunks
-chunk_num = np.prod([len(i) for i in chunk_sizes])
-
-with rasterio.open(output_path, 'w', **meta) as dst:
-    pbar = tqdm(total=chunk_num)
-    # Loop through the chunks
-    for y_chunk, y_start in zip(chunk_sizes[0], np.cumsum([0] + list(chunk_sizes[0][:-1]))):
-        for x_chunk, x_start in zip(chunk_sizes[1], np.cumsum([0] + list(chunk_sizes[1][:-1]))):
-            # Get the chunk data
-            chunk = arr.isel(y=slice(y_start, y_start + y_chunk), x=slice(x_start, x_start + x_chunk)).data
-            dst.write(chunk, 1, window=((y_start, y_start + y_chunk), (x_start, x_start + x_chunk)))
-            pbar.update(1)
-
+calculate_area(path, chunk_size=work_size, output_path=output_path)
 
 
 
