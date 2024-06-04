720afd10b17262a1956dd8a12832816d6ea37673
jinzhu <wangjinzhulala@gmail.com>
2024-06-04 16:50:14 +1000

    Finish calculating the urban occupy framland at GAEZ scale

--- 13_Get_Urban_occupying_cropland_area.py
+++ 13_Get_Urban_occupying_cropland_area.py
@@ -1,17 +1,15 @@
 import numpy as np
 import xarray as xr
-import rioxarray as rxr
 
 from collections import defaultdict
 from itertools import product
-from dask.diagnostics import ProgressBar
 from rasterio.enums import Resampling
 from tqdm.auto import tqdm
 from joblib import Parallel, delayed
 
 
-# Uncoarsen GAEZ mask to the same resolution as LUCC mask
-GAEZ_mask = xr.open_dataset('data/GAEZ_v4/GAEZ_mask.tif')
+# # Uncoarsen GAEZ mask to the same resolution as LUCC mask
+# GAEZ_mask = xr.open_dataset('data/GAEZ_v4/GAEZ_mask.tif')
 # GAEZ_cell_id = np.arange(GAEZ_mask['band_data'].size).reshape(GAEZ_mask['band_data'].shape).astype('int64')
 # GAEZ_mask['id'] = xr.DataArray(GAEZ_cell_id, coords=GAEZ_mask['band_data'].coords)
 # GAEZ_mask = GAEZ_mask.chunk({'y': 160, 'x': 149}).drop_vars('band_data')
@@ -30,6 +28,7 @@ GAEZ_mask = xr.open_dataset('data/GAEZ_v4/GAEZ_mask.tif')
 
 
 # Read data
+GAEZ_mask = xr.open_dataset('data/GAEZ_v4/GAEZ_mask.tif')
 urban_cells = xr.open_dataset('data/results/step_12_urban_potential_arr_reclass.nc', chunks='auto')['data']
 lucc_area = xr.open_dataset('data/LUCC/LUCC_Area_km2.nc', chunks='auto')['data']
 
@@ -48,20 +47,21 @@ GAEZ_uncoarsened = GAEZ_uncoarsened.chunk({
     if k in ['band', 'y', 'x']})
 
 
-
+# Calculate the area of cropland occupied by urban expansion
 def bincount_chunked(arr, ssp, year, msk):
     return (ssp,year), np.bincount(msk.compute().flatten(), weights=arr.compute().flatten(), minlength=GAEZ_mask['band_data'].size)
 
 
-paralle_obj = Parallel(n_jobs=-1,  return_as='generator')
-
-    
+# Wrap the calculation into delayed objects
 tasks = []
 for ssp, year in list(product(urban_occupy_cropland['ssp'].data, urban_occupy_cropland['year'].data)):
     for msk, arr in zip(GAEZ_uncoarsened.data.to_delayed().flatten(), 
                         urban_occupy_cropland.sel(ssp=ssp, year=year).data.to_delayed().flatten()): 
         tasks.append(delayed(bincount_chunked)(arr, ssp, year, msk))
 
+
+# Loop over the delayed objects and calculate the area of cropland occupied by urban expansion
+paralle_obj = Parallel(n_jobs=-1,  return_as='generator')
 
 outputs = defaultdict(list)
 with tqdm(total=len(tasks)) as pbar:
@@ -70,8 +70,23 @@ with tqdm(total=len(tasks)) as pbar:
         pbar.update()           
 
 
+# Combine the results as a xarray
+arrs = []
+for (ssp,year), val in outputs.items():
+    arr_sum = np.array(val).sum(axis=0)
+    arr_sum = arr_sum.reshape(GAEZ_mask['band_data'].shape)
+    arr_sum = xr.DataArray(arr_sum, coords={k:GAEZ_mask['band_data'].coords[k] for k in ['band', 'y', 'x']})
+    arr_sum = arr_sum.expand_dims({'ssp': [ssp], 'year': [year]})
+    arr_sum = arr_sum.rio.write_crs(GAEZ_mask.rio.crs)
+    
+    arrs.append(arr_sum)
+    
 
+# Save the results
+encoding = {'data': {'dtype': 'float32', 'zlib': True, 'complevel': 9}}
 
+urban_occupy_cropland = xr.combine_by_coords(arrs)
+urban_occupy_cropland.name = 'data'
+urban_occupy_cropland.to_netcdf('data/results/step_13_urban_occupy_cropland.nc', encoding=encoding, engine='h5netcdf')
 
-
-
+    
