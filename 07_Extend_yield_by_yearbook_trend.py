Fix the mean_mask_province error in reclimation calculation

07_Extend_yield_by_yearbook_trend.py
@@ -32,7 +32,8 @@ yearbook_trend = xr.open_dataset('data/results/step_6_yearbook_yield_extrapolate
 
 mask_sum = rxr.open_rasterio('data/GAEZ_v4/Province_mask.tif')
 mask_mean = rxr.open_rasterio('data/GAEZ_v4/Province_mask_mean.tif')
-mask_province = [xr.where(mask_sum == idx, 1, 0).expand_dims({'Province': [p]}) for idx,p in enumerate(UNIQUE_VALUES['Province'])]
+mask_province = [xr.where(mask_sum == idx, 1, 0).expand_dims({'Province': [p]}) 
+                 for idx,p in enumerate(UNIQUE_VALUES['Province'])]
 mask_province = xr.combine_by_coords(mask_province).astype('float32') 
 
 GAEZ_attainable_yield = xr.open_dataset('data/results/step_3_GAEZ_AY_GYGA_mean.nc', chunks='auto')['data']/1000     # kg/ha -> t/ha
@@ -56,7 +57,6 @@ GAEZ_MC = xr.DataArray(
     )
 
 
-
 # Sample from yearbook trend
 Yearbook_MC = sample_ppf(yearbook_trend['mean'], yearbook_trend['std'], n_samples=Monte_Carlo_num)
 Yearbook_MC = xr.DataArray(
@@ -64,6 +64,7 @@ Yearbook_MC = xr.DataArray(
     dims=('sample',) + yearbook_trend['mean'].dims, 
     coords={'sample':range(Monte_Carlo_num), **yearbook_trend['mean'].coords}
     )
+
 
 # Assign ratio value to mask, so all pixels inside a province have the same value
 Yearbook_MC = Yearbook_MC * mask_province
@@ -75,13 +76,14 @@ Yearbook_MC_ratio = Yearbook_MC / Yearbook_MC.sel(year=2020)
 # Get the year when yield touches the attainable ceiling
 Yield_MC = GAEZ_MC * Yearbook_MC_ratio
 Yield_MC_mean = Yield_MC.mean(dim='sample')
-Yield_MC_stats = bincount_with_mask(mask_sum, Yield_MC_mean * mask_mean)
 
+Yield_MC_stats = bincount_with_mask(mask_sum, Yield_MC_mean * mask_mean)
 Attainable_stats = bincount_with_mask(mask_sum, GAEZ_attainable_yield * mask_mean)
 
-Yield_with_Attain = Yield_MC_stats.merge(Attainable_stats, 
-                                         on=['bin', 'crop', 'water_supply', 'year', 'rcp', 'c02_fertilization'],
-                                         suffixes=('_yield', '_attainable'))
+Yield_with_Attain = Yield_MC_stats.merge(
+    Attainable_stats, 
+    on=['bin', 'crop', 'water_supply', 'year', 'rcp', 'c02_fertilization'],
+    suffixes=('_yield', '_attainable'))
 
 Yield_with_Attain['diff'] = abs(Yield_with_Attain['Value_yield'] - Yield_with_Attain['Value_attainable'])
 
@@ -131,6 +133,7 @@ if __name__ == '__main__':
     plotnine.options.figure_size = (16, 6)
     plotnine.options.dpi = 100
     
+    # Filter the data with rcp and c02_fertilization
     rcp = 'RCP2.6'
     co2 = 'With CO2 Fertilization'
     
@@ -174,14 +177,17 @@ if __name__ == '__main__':
     GAEZ_yb_mean_stats = bincount_with_mask(mask_sum, GAEZ_yb_mean * mask_mean)
     GAEZ_yb_std_stats = bincount_with_mask(mask_sum, GAEZ_yb_std * mask_mean)
     
-    GAEZ_yb_stats = GAEZ_yb_mean_stats.merge(GAEZ_yb_std_stats,
-                                             on = ['bin', 'crop', 'water_supply', 'year', 'rcp', 'c02_fertilization'], 
-                                             suffixes = ('_mean', '_std'))
-    GAEZ_yb_stats = GAEZ_yb_stats.rename(columns = {
-        'bin': 'Province', 
-        'Value_mean': 'Yield_mean (t/ha)', 
-        'Value_std': 'Yield_std (t/ha)'
-        })
+    GAEZ_yb_stats = GAEZ_yb_mean_stats.merge(
+        GAEZ_yb_std_stats,
+        on = ['bin', 'crop', 'water_supply', 'year', 'rcp', 'c02_fertilization'], 
+        suffixes = ('_mean', '_std'))
+    
+    GAEZ_yb_stats = GAEZ_yb_stats.rename(
+        columns = {
+            'bin': 'Province', 
+            'Value_mean': 'Yield_mean (t/ha)', 
+            'Value_std': 'Yield_std (t/ha)'
+            })
     
     GAEZ_yb_stats['Province'] = GAEZ_yb_stats['Province'].map(dict(enumerate(UNIQUE_VALUES['Province'])))
     

14_Get_reclimation_increased_framland_area.py
@@ -1,21 +1,29 @@
+import math
+import numpy as np
 import pandas as pd
 import rioxarray as rxr
 import xarray as xr
 import statsmodels.api as sm
+import plotnine
 
 from helper_func import read_yearbook, sample_ppf
+from helper_func.calculate_GAEZ_stats import bincount_with_mask
 from helper_func.parameters import BASE_YR, UNIQUE_VALUES, Monte_Carlo_num
 
 
 # Read data
+mask_sum = rxr.open_rasterio('data/GAEZ_v4/Province_mask.tif', chunks='auto')
+mask_province = [xr.where(mask_sum == idx, 1, 0).expand_dims({'Province': [p]}) 
+                      for idx,p in enumerate(UNIQUE_VALUES['Province'])]
+mask_province = xr.concat(mask_province, dim='Province').astype('int8')
+
 mask_mean = rxr.open_rasterio('data/GAEZ_v4/Province_mask_mean.tif', chunks='auto')
-mask_mean_province = [xr.where(mask_mean == idx, 1, 0).expand_dims({'Province': [p]}) for idx,p in enumerate(UNIQUE_VALUES['Province'])]
-mask_mean_province = xr.combine_by_coords(mask_mean_province).astype('float32')
+mask_mean_province = mask_province * mask_mean
 
 
-reclimation = read_yearbook('data/Yearbook/yearbook_land_reclimation_total_area_ha.csv', 'Reclimation') # ha
+reclimation = read_yearbook('data/Yearbook/yearbook_land_reclimation_total_area_ha.csv', 'Reclimation')     # ha
 reclimation = reclimation.sort_values(['Province','year']).reset_index(drop=True)
-reclimation['Cumsum Area (km2)'] = reclimation.groupby('Province')['Value'].cumsum()/1e4                # km2
+reclimation['Cumsum Area (km2)'] = reclimation.groupby('Province')['Value'].cumsum()/1e4                    # km2
 
 
 # Build model for each province
@@ -43,6 +51,11 @@ for idx, df in reclimation.groupby('Province'):
 # Make sure the order is correct
 prediction_df = prediction_df.sort_values(['Province','year'])
 
+# Subtract values for BASE_YR to align with the beginning of the modeling period
+prediction_df['mean km2'] = prediction_df.groupby('Province').transform(lambda x: x - x.iloc[0])['mean km2']
+prediction_df['std km2'] = np.min(prediction_df[['std km2','mean km2']].values + 1e-5, axis=1)     # Std must > mean to avoide negative values
+
+
 # Sample from mean and std of reclimation area for each province
 reclimation_sample = sample_ppf(
     prediction_df['mean km2'], 
@@ -50,7 +63,8 @@ reclimation_sample = sample_ppf(
     n_samples=Monte_Carlo_num)
 
 reclimation_sample = xr.DataArray(
-    reclimation_sample.reshape(
+    name='data',
+    data=reclimation_sample.reshape(
         Monte_Carlo_num,
         prediction_df['Province'].nunique(), 
         prediction_df['year'].nunique()),
@@ -70,3 +84,38 @@ reclimation_sample_cell = reclimation_sample_cell.sum(dim='Province')
 encoding = {'data': {'zlib': True, 'complevel': 9}}
 reclimation_sample_cell.name = 'data'
 reclimation_sample_cell.to_netcdf('data/results/step_14_reclimation_area_km2.nc', encoding=encoding)
+
+
+
+if __name__ == '__main__':
+    plotnine.options.figure_size = (12, 8)
+    plotnine.options.dpi = 100
+
+    reclimation_mean = reclimation_sample_cell.mean(dim='sample')
+    reclimation_std = reclimation_sample_cell.std(dim='sample')
+    reclimation_mean_stats = bincount_with_mask(mask_sum, reclimation_mean)
+    reclimation_std_stats = bincount_with_mask(mask_sum, reclimation_std)
+    
+    reclimation_stats = reclimation_mean_stats.merge(
+        reclimation_std_stats, 
+        on=['bin', 'year'], 
+        suffixes=('_mean', '_std'))
+    
+    reclimation_stats = reclimation_stats.rename(
+        columns={'Value_mean':'mean', 'Value_std':'std', 'bin':'Province'})
+    
+    reclimation_stats['Province'] = reclimation_stats['Province'].map({
+        idx:p for idx,p in enumerate(UNIQUE_VALUES['Province'])
+        })
+    
+    reclimation_stats['lower'] = reclimation_stats['mean'] - (reclimation_stats['std'] / math.sqrt(Monte_Carlo_num) * 1.96)
+    reclimation_stats['upper'] = reclimation_stats['mean'] + (reclimation_stats['std'] / math.sqrt(Monte_Carlo_num) * 1.96)
+    
+    g = (plotnine.ggplot(reclimation_stats) +
+         plotnine.geom_line(plotnine.aes(x='year', y='mean', color='Province')) +
+         plotnine.geom_ribbon(plotnine.aes(x='year', ymin='lower', ymax='upper', fill='Province'), alpha=0.5) +
+         plotnine.theme_bw() +
+         plotnine.theme(legend_position='none') +
+         plotnine.labs(x='Year', y='Reclimation Area (km2)'))
+
+    