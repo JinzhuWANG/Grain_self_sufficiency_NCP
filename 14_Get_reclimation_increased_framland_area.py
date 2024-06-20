Correct calculation on reclimation area

14_Get_reclimation_increased_framland_area.py
@@ -1,4 +1,5 @@
 import math
+from re import S
 import numpy as np
 import pandas as pd
 import rioxarray as rxr
@@ -8,7 +9,7 @@ import plotnine
 
 from helper_func import read_yearbook, sample_ppf
 from helper_func.calculate_GAEZ_stats import bincount_with_mask
-from helper_func.parameters import BASE_YR, UNIQUE_VALUES, Monte_Carlo_num
+from helper_func.parameters import BASE_YR, PRED_STEP, UNIQUE_VALUES, Monte_Carlo_num
 
 
 # Read data
@@ -21,9 +22,9 @@ mask_mean = rxr.open_rasterio('data/GAEZ_v4/Province_mask_mean.tif', chunks='aut
 mask_mean_province = mask_province * mask_mean
 
 
-reclimation = read_yearbook('data/Yearbook/yearbook_land_reclimation_total_area_ha.csv', 'Reclimation')     # ha
+reclimation = read_yearbook('data/Yearbook/yearbook_land_reclimation_cropland_increase_ha.csv', 'Reclimation')     # ha
 reclimation = reclimation.sort_values(['Province','year']).reset_index(drop=True)
-reclimation['Cumsum Area (km2)'] = reclimation.groupby('Province')['Value'].cumsum()/1e4                    # km2
+reclimation['Cumsum Area (km2)'] = reclimation.groupby('Province')['Value'].cumsum()/1e2                    # ha -> km2
 
 
 # Build model for each province
@@ -36,7 +37,7 @@ for idx, df in reclimation.groupby('Province'):
     model = sm.OLS(y, X).fit()
     
     # Predict
-    pred_df = pd.DataFrame({'year':range(BASE_YR,2101)})
+    pred_df = pd.DataFrame({'year':range(BASE_YR, 2101, PRED_STEP)})
     pred_df = sm.add_constant(pred_df)
 
     inf_df = model.get_prediction(pred_df).summary_frame(alpha=0.32) # 0.68 CI indicates the mean+/-std
@@ -88,8 +89,8 @@ reclimation_sample_cell.to_netcdf('data/results/step_14_reclimation_area_km2.nc'
 
 
 if __name__ == '__main__':
-    plotnine.options.figure_size = (6, 4)
-    plotnine.options.dpi = 100
+    plotnine.options.figure_size = (10, 6)
+    plotnine.options.dpi = 200
 
     reclimation_mean = reclimation_sample_cell.mean(dim='sample')
     reclimation_std = reclimation_sample_cell.std(dim='sample')
@@ -111,10 +112,18 @@ if __name__ == '__main__':
     reclimation_stats['lower'] = reclimation_stats['mean'] - (reclimation_stats['std'] / math.sqrt(Monte_Carlo_num) * 1.96)
     reclimation_stats['upper'] = reclimation_stats['mean'] + (reclimation_stats['std'] / math.sqrt(Monte_Carlo_num) * 1.96)
     
-    g = (plotnine.ggplot(reclimation_stats) +
-         plotnine.geom_line(plotnine.aes(x='year', y='mean', color='Province')) +
-         plotnine.geom_ribbon(plotnine.aes(x='year', ymin='lower', ymax='upper', fill='Province'), alpha=0.5) +
+    g = (plotnine.ggplot() +
+         plotnine.geom_point(
+             reclimation, 
+             plotnine.aes(x='year', y='Cumsum Area (km2)'), color='grey', size=0.03) +
+         plotnine.geom_line(
+             reclimation_stats, 
+             plotnine.aes(x='year', y='mean', color='Province')) +
+         plotnine.geom_ribbon(
+             reclimation_stats,
+             plotnine.aes(x='year', ymin='lower', ymax='upper', fill='Province'), alpha=0.5) +
          plotnine.theme_bw() +
+         plotnine.facet_wrap('~Province', scales='free_y') +
          plotnine.labs(x='Year', y='Reclimation Area (km2)'))
     
     g.save('data/results/fig_step_14_cropland_reclimation_prediction_cumsum_km2.svg')

16_Multi_yield_area_for_production.py
@@ -1,9 +1,11 @@
+import math
 import xarray as xr
 import rioxarray as rxr
 import pandas as pd
+import plotnine
 
 from helper_func.calculate_GAEZ_stats import bincount_with_mask
-from helper_func.parameters import UNIQUE_VALUES
+from helper_func.parameters import UNIQUE_VALUES, Monte_Carlo_num
 
 # Get masks
 mask = rxr.open_rasterio('data/GAEZ_v4/GAEZ_mask.tif')
@@ -19,13 +21,16 @@ yield_MC_actual = xr.open_dataset('data/results/step_7_GAEZ_yield_MC.nc', chunks
 yield_pred = yield_MC_actual * yield_MC_yrbook
 
 
+
 area_start = xr.open_dataset('data/results/step_15_GAEZ_area_km2_adjusted.nc', chunks='auto')['data']   # km2
 area_start = area_start * mask_province
 area_crop_water_ratio = area_start.sum(dim=['y', 'x', 'band']) / area_start.sum(dim=['crop', 'water_supply', 'y', 'x', 'band'])
+area_crop_water_ratio = area_crop_water_ratio * mask_province       # Assing the multiplier to each pixel in the given Province
 
-area_urban_reduce = xr.open_dataset('data/results/step_13_urban_occupy_cropland.nc', chunks='auto')['data']
+
+area_urban_reduce = xr.open_dataset('data/results/step_13_urban_occupy_cropland.nc', chunks='auto')['data'] # km2
 area_urban_reduce = area_urban_reduce * area_crop_water_ratio
-area_reclaim_increase = xr.open_dataset('data/results/step_14_reclimation_area_km2.nc', chunks='auto')['data']
+area_reclaim_increase = xr.open_dataset('data/results/step_14_reclimation_area_km2.nc', chunks='auto')['data'] # km2
 area_reclaim_increase = area_reclaim_increase * area_crop_water_ratio
 
 area_pred = area_start + area_reclaim_increase - area_urban_reduce
@@ -33,17 +38,48 @@ area_pred = area_pred.sum(dim=['Province'])
 
 
 # Get production
-production_pred = yield_pred * area_pred /1e4                           # million tonnes
-production_pred_mean = production_pred.mean(dim=['sample'])
-production_pred_std = production_pred.std(dim=['sample'])
-
-production_mean_stats = bincount_with_mask(mask_sum, production_pred_mean)
-production_std_stats = bincount_with_mask(mask_sum, production_pred_std)
-production_stats = pd.merge(
-    production_mean_stats, 
-    production_std_stats, 
-    on = [ 'rcp', 'ssp', 'year', 'bin', 'crop', 'water_supply', 'c02_fertilization'], 
-    suffixes = ('_mean', '_std'))
-production_stats = production_stats.rename(columns = {'Value_mean': 'Production (tonnes)', 'Value_std': 'Production_std'})
+production_pred = yield_pred * area_pred * 100 / 1e6                      # million tonnes
 
 
+if __name__ == '__main__':
+    
+    plotnine.options.figure_size = (18, 6)
+    plotnine.options.dpi = 100
+    
+
+    production_pred_mean = production_pred.mean(dim=['sample'])
+    production_pred_std = production_pred.std(dim=['sample'])
+
+    production_mean_stats = bincount_with_mask(mask_sum, production_pred_mean)
+    production_std_stats = bincount_with_mask(mask_sum, production_pred_std)
+    
+    production_stats = pd.merge(
+        production_mean_stats, 
+        production_std_stats, 
+        on = [ 'rcp', 'ssp', 'year', 'bin', 'crop', 'water_supply', 'c02_fertilization'], 
+        suffixes = ('_mean', '_std'))
+    production_stats = production_stats.rename(
+        columns = {'Value_mean': 'Production (Mt)', 'Value_std': 'Production_std (Mt)', 'bin': 'Province'})
+    production_stats = production_stats.groupby(['rcp', 'ssp', 'year', 'Province', 'crop','c02_fertilization']).sum().reset_index()
+    
+    production_stats['Province'] = production_stats['Province'].map(dict(enumerate(UNIQUE_VALUES['Province'])))
+    production_stats['lower'] = production_stats['Production (Mt)'] - (production_stats['Production_std (Mt)'] / math.sqrt(Monte_Carlo_num)) * 1.96
+    production_stats['upper'] = production_stats['Production (Mt)'] + (production_stats['Production_std (Mt)'] / math.sqrt(Monte_Carlo_num)) * 1.96
+    
+    # Filter data for plotting
+    rcp = 'RCP2.6'
+    ssp = 'SSP2'
+    
+    plot_df = production_stats.query(f'rcp == "{rcp}" & ssp == "{ssp}"').copy()
+    
+    g = (
+        plotnine.ggplot() +
+        plotnine.geom_line(
+            plot_df, 
+            plotnine.aes(x='year', y='Production (Mt)', color='c02_fertilization')) +
+        plotnine.geom_ribbon(
+            plot_df, 
+            plotnine.aes(x='year', ymin='lower', ymax='upper', fill='c02_fertilization'), alpha=0.3) +
+        plotnine.facet_grid('crop~Province', scales='free_y') +
+        plotnine.theme_bw()
+    )

helper_func/__init__.py
@@ -110,13 +110,15 @@ def fit_linear_model(df):
     return extrapolate_df
 
 
-def sample_ppf(mean, std, n_samples=100):
-
+def sample_ppf(mean, std, n_samples=100, seed=0):
+    np.random.seed(seed)
+    
     # Get a uniform distribution between 0.001 and 0.999
     samples = np.random.uniform(0.001, 0.999, n_samples)
     
-    # Calculate the percent point function (inverse of the CDF)
+    # Use a consistent random seed within the parallel execution
     def calculate_ppf(x):
+        np.random.seed(seed)  # Ensure the seed is set for each worker
         return norm.ppf(x, loc=mean, scale=std)
     
     tasks = [delayed(calculate_ppf)(n) for n in samples]